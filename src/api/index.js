6163ca67e69be42c2fa46970fb6706a36edc6c6f
dario888 <dario.bario87@hotmail.com>
2020-05-10 14:16:23 +0200

    Creating Cards Commit 4

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 export const fetchData = async () => {
    try {
-    const res = await axios.get('https://covid19.mathdro.id/api');
-    return res
+    const {data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get('https://covid19.mathdro.id/api');
+    return  {confirmed, recovered, deaths, lastUpdate}
        
    } catch (error) {
        
